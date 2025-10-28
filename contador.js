6940a84ae460103622c0ec8dbaa9daef81ed5215
Marcus Vinicius <marcusvinicius.rj45@gmail.com>
2025-10-28 18:14:27 -0300

    Update contador.js

--- contador.js
+++ contador.js
@@ -1,35 +1,40 @@
-function contar(){ //documento de chamada no HTML
-   //variaveis globais
-  let ini = document.getElementById('txti')
-  let fim = document.getElementById('txtf')
-  let passo document.getElementById('txtp')
-  let res = document.getElementById('res')
-  
-  //estrutura de repetição
-  if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-    res.innerHTML = 'Não é possível contar!'
-    //window.alert('[ERRO] Cade os dados?')
-  } else {
-    res.innerHTML = 'contando:' <br>
-    let i = Number(ini.value)
-    let f = Number(fim.value)
-    let p = Number(passo.value)
-    if (p <= 0){
-      window.alert('Passo inválido! Considere o PASSO 1')
-      p = 1
-      }
-      //Contagem crescente
-      if (i < f){ 
-      for(let c = i; c <= f; c += p){
-        res.innerHTML += ` ${c} \u{1F47E} `
+function contar() { // documento de chamada no HTML
+    // variaveis globais
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp') 
+    let res = document.getElementById('res')
+    
+    // estrutura de repetição
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Não é possível contar!'
+        //window.alert('[ERRO] Cade os dados?')
+    } else {
+        //concatenação de string
+        res.innerHTML = 'Contando: <br>' 
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        
+        if (p <= 0){
+            window.alert('Passo inválido! Considere o PASSO 1')
+            p = 1
         }
-      }else {
-        // contatem regressiva
-        for(let c i; c >= f; c-=p){
-          res.innerHTML += `${c} \u{1F449}`
+        
+        // Contagem crescente
+        if (i < f){ 
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += ` ${c} \u{1F449} ` // Mudei o emoji para o de seta
+            }
+        } else {
+            // Contagem regressiva
+            //inicialização do 'c'
+            for(let c = i; c >= f; c -= p){ 
+                res.innerHTML += `${c} \u{1F449}`
+            }
         }
-      }
+        
+        // O emoji de bandeira (FIM) deve vir depois do loop, indicando o final.
+        res.innerHTML += `\u{1F3C1}` 
     }
-    res.innterHTML += `\u{1F3C1}`
-    }
-  }
+} 
